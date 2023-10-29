6399930b1ef93fac8602372b58b406006ce8e967
Breno <breno.givas@icloud.com>
2023-10-29 04:24:48 -0300

    feat: product's carousel and produc's page

--- js/product.js
+++ js/product.js
@@ -1,8 +1,7 @@
-const card = document.querySelectorAll('.carousel-item')
-const productPrice = document.querySelectorAll('.product-info h3')
+const productContainers = document.querySelectorAll('.carousel')
 
-function fetchCardData(path) {
-  fetch(`${path}`)
+function fetchAndPopulateData(container, jsonFile) {
+  fetch(jsonFile)
     .then((response) => {
       if (!response.ok) {
         throw new Error('Erro na requisição fetch')
@@ -10,15 +9,17 @@ function fetchCardData(path) {
       return response.json()
     })
     .then((data) => {
-      card.forEach((box, index) => {
+      const cards = container.querySelectorAll('.carousel-item')
+      cards.forEach((box, index) => {
         const keys = Object.keys(data)
         const img = box.querySelector('img')
-        const productTitle = box.querySelector('.product-info a h2')
+        const productTitle = box.querySelector('.product-info a')
         const productPrice = box.querySelector('.product-info h3')
         const productData = data[keys[0]]
         if (productData && index < 5) {
           img.src = productData[index + 1].Imagem
           productTitle.textContent = productData[index + 1].Nome
+          productTitle.href = productData[index + 1].Link
           productPrice.textContent = productData[index + 1].Historico[1]
         }
       })
@@ -28,4 +29,6 @@ function fetchCardData(path) {
     })
 }
 
-fetchCardData('./data/produtosTERABYTE.json')
+fetchAndPopulateData(productContainers[0], './data/produtosTERABYTE.json')
+fetchAndPopulateData(productContainers[1], './data/produtosKABUMTESTE.json')
+fetchAndPopulateData(productContainers[2], './data/produtosPiCHAUTESTE.json')
